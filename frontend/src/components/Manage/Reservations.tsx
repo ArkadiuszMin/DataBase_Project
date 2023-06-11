b6365ca19c42ae46d9858b2156f9bed7651cb9b9
AnCichocka <annacichocka.poczta@gmail.com>
2023-06-11 12:57:25 +0200

    adding reservation done

--- frontend/src/components/Manage/Reservations.tsx
+++ frontend/src/components/Manage/Reservations.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Reservations = () => {
-  return <div>Reservations</div>;
+  const [reservations, setReservations] = useState<Adoption[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/adoptions/toConfirm")
+      .then((response) => {
+        setReservations(response.data), setIsLoaded(true);
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle error, set reservations state to an empty array or display an error message
+        console.log("error!");
+      });
+  }, []);
+
+  function confirmReservation(adoptionId: string): void {
+    axios
+      .get("http://localhost:8080/adoptions/confirm?id=" + adoptionId)
+      .then((response) => {
+        console.log("RESPONSE: " + response.data);
+
+        // Filter out the confirmed reservation from the reservations list
+        setReservations((prevReservations) =>
+          prevReservations.filter(
+            (reservation) => reservation.adoptionId !== adoptionId
+          )
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  return (
+    <>
+      <h2>Reservations</h2>
+      {reservations.map((reservation: Adoption) => (
+        <div key={reservation.adoptionId}>
+          <p>{reservation.adoptionId}</p>
+          <button onClick={() => confirmReservation(reservation.adoptionId)}>
+            confirm
+          </button>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Reservations;
