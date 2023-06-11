17ddc289cb876b429bca61de2567d8cd09cbc17c
tudde <tuddeum@gmail.com>
2023-06-11 14:59:19 +0200

    duplicate adopters not being added + fixed CORS at /adopters

--- backend/Spring/SpringAPI/src/main/java/com/example/springapi/api/controller/AdopterController.java
+++ backend/Spring/SpringAPI/src/main/java/com/example/springapi/api/controller/AdopterController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/adopters")
+@CrossOrigin("http://localhost:5173")
 public class AdopterController {
 
     private final AdopterService adopterService;
