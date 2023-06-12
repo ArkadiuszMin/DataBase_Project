5e61e230f75b046c41f35da2728d58e1bdd8615b
tudde <tuddeum@gmail.com>
2023-06-13 01:48:54 +0200

    added exceptions in adoption and adopter controllers

--- backend/Spring/SpringAPI/src/main/java/com/example/springapi/api/controller/AdopterController.java
+++ backend/Spring/SpringAPI/src/main/java/com/example/springapi/api/controller/AdopterController.java
@@ -1,9 +1,12 @@
 package com.example.springapi.api.controller;
 
 import com.example.springapi.api.model.Adopter;
+import com.example.springapi.api.model.Adoption;
 import com.example.springapi.service.AdopterService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,12 +24,24 @@ public class AdopterController {
         this.adopterService = adopterService;
     }
     @GetMapping("/all")
-    public List<Adopter> getAllAdopters(){
-        return adopterService.getAllAdopters();
+    public ResponseEntity<List<Adopter>> getAllAdopters(){
+        try{
+            return new ResponseEntity<>(adopterService.getAllAdopters(), HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
     }
     @GetMapping("/byId")
-    public Adopter getAdopterById(@RequestParam String id){
-        Optional<Adopter> adopter = adopterService.getAdopterById(id);
-        return adopter.orElse(null);
+    public ResponseEntity<Adopter> getAdopterById(@RequestParam String id){
+        try{
+            Optional<Adopter> adopter = adopterService.getAdopterById(id);
+            if(adopter.isEmpty()){
+                return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(adopter.get(),HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
