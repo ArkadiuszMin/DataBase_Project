b74516f5e223d053c59293f1741990999ea1c064
tudde <tuddeum@gmail.com>
2023-06-13 20:49:12 +0200

    fixed adopter error handling

--- backend/Spring/SpringAPI/src/main/java/com/example/springapi/service/AdopterService.java
+++ backend/Spring/SpringAPI/src/main/java/com/example/springapi/service/AdopterService.java
@@ -1,8 +1,12 @@
 package com.example.springapi.service;
 
+import com.example.springapi.api.enums.State;
 import com.example.springapi.api.model.Adopter;
+import com.example.springapi.api.model.Adoption;
 import com.example.springapi.api.repository.AdopterRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,12 +16,23 @@ import java.util.Optional;
 @AllArgsConstructor
 public class AdopterService {
     private final AdopterRepository adopterRepository;
-
-    public List<Adopter> getAllAdopters(){
-        return adopterRepository.findAll();
+    public ResponseEntity<List<Adopter>> getAllAdopters(){
+        try {
+            return new ResponseEntity<>(adopterRepository.findAll(), HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+    public ResponseEntity<Adopter> getAdopterById(String Id){
+        try{
+            Optional<Adopter> adopter =  adopterRepository.findAdopterById(Id);
+            if(adopter.isEmpty()){
+                return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(adopter.get(),HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
-    public Optional<Adopter> getAdopterById(String Id){
-        return adopterRepository.findAdopterById(Id);
     }
 }
